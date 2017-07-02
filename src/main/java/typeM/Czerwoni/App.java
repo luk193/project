4c7438e0f3acb71bb9df8c9d8a9acf0faf3c6523
djaracz <dawid.jaracz@gmail.com>
2017-07-02 11:32:28 +0200

    base report

--- src/main/java/typeM/Czerwoni/App.java
+++ src/main/java/typeM/Czerwoni/App.java
@@ -1,6 +1,10 @@
 package typeM.Czerwoni;
 
 import java.util.ArrayList;
+import java.util.Date;
+import de.vandermeer.asciitable.AsciiTable;
+import typeM.Czerwoni.model.Record;
+import typeM.Czerwoni.model.Row;
 
 /**
  * Hello world!
@@ -10,11 +14,17 @@ public class App
 {
     public static void main( String[] args ) throws Exception
     {
-        Excel ss = new Excel();
-        ss.readXLS("sss");
-        Menu menu = new Menu(args[0]);
-        menu.printMenu();
+    	
+    	
+//        Excel ss = new Excel();
+//        ss.readXLS("sss");
+//        Menu menu = new Menu(args[0]);
+//        menu.printMenu();
         
+    	BaseReport tab = new BaseReport();
+        tab.addRow(new Row("fdfas", "zz"));
+        tab.addRow(new Row("fdfas", "zz"));
+        tab.printTable();
     }
-        
+    
 }
