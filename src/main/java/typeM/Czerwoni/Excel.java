e5a066851ac5e3941db5ce75be7b94f89009bcae
luk193 <LC@gmail.com>
2017-07-02 10:33:04 +0200

    pierwsza wersja excel

--- src/main/java/typeM/Czerwoni/Excel.java
+++ src/main/java/typeM/Czerwoni/Excel.java
@@ -1,37 +1,76 @@
 package typeM.Czerwoni;
 
 import java.io.FileInputStream;
+import java.util.ArrayList;
 
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 
+import typeM.Czerwoni.model.Record;
+
 public class Excel {
 
-	public void readXLS(String path_xls) throws Exception {
-
-		HSSFSheet sheet;
-		POIFSFileSystem fs = new POIFSFileSystem(new FileInputStream("C:\\Users\\student\\Desktop\\Kowalski_Jan.xls"));
-		HSSFWorkbook wb = new HSSFWorkbook(fs);
-		sheet = wb.getSheetAt(0);
-		// HSSFRow row = sheet.getRow(0);
-
-	
-	
+	public void readXLS(ArrayList<String> Sciezki_plikow_xls) throws Exception {
 		
-			
-			for (int u = 1; u < 200; u++) {
-			
-				
-				
-				if (sheet.getRow(u)!= null) {
-				if(sheet.getRow(u).getCell(0) != null && sheet.getRow(u).getCell(1) != null && sheet.getRow(u).getCell(2) != null) {
+		ArrayList<Record> All_data = new ArrayList<>();
+		
 
-			System.out.println("" + sheet.getRow(u).getCell(0).toString());
-			System.out.println("" + sheet.getRow(u).getCell(1).toString());
-			System.out.println("" + sheet.getRow(u).getCell(2).toString());
-				}}}
+		for (String path : Sciezki_plikow_xls) {
+			
+			
+			
+
+			POIFSFileSystem fs = new POIFSFileSystem(new FileInputStream(path));
+			HSSFWorkbook wb = new HSSFWorkbook(fs);
+
+			HSSFSheet sheet;
+			int i = 0;
+			while (wb.getSheetAt(i) != null) {
+				double h = 0;
+				String year;
+				String month;
+				
+				
+				Record simple_record = new Record();
+				sheet = wb.getSheetAt(i);
+				// HSSFRow row = sheet.getRow(0);
+				
+				simple_record.setProject(sheet.getSheetName());
+				
+				//nazwisko i imie do zaimplementowania
+
+				for (int u = 1; u < 200; u++) {
+
+					try {
+						h=  h + sheet.getRow(u).getCell(2).getNumericCellValue();
+						
+					}
+					catch(Exception e) {
+						
+					}
+					
+					simple_record.setHours(h);
+					
+					
+					
+				/*	if (sheet.getRow(u) != null) {
+						if (sheet.getRow(u).getCell(0) != null && sheet.getRow(u).getCell(1) != null
+								&& sheet.getRow(u).getCell(2) != null) {
+
+							System.out.println("" + sheet.getRow(u).getCell(0).toString());
+							System.out.println("" + sheet.getRow(u).getCell(1).toString());
+							System.out.println("" + sheet.getRow(u).getCell(2).toString());
+							
+							
+						}
+					}*/
+				}
+				i++;
+				All_data.add(simple_record);
+			}
+			
 		}
+	}
 
-	
 }
