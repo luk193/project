ea1310feee5245232ad51d15c340a1db2af0533c
bartekelm <dfs@op.pl>
2017-07-02 12:12:06 +0200

    manu major brake app changes

--- src/main/java/typeM/Czerwoni/App.java
+++ src/main/java/typeM/Czerwoni/App.java
@@ -10,7 +10,27 @@ public class App
 {
     public static void main( String[] args ) throws Exception
     {
+    	
+    	Excel ss = new Excel();
+        
+        Timesheets timesheets = new Timesheets();
+        ArrayList<String> paths = timesheets.getListOfPaths(args[0]);
+        
+        System.out.println("Witamy w systemie!");
+        System.out.println("Wczytane raporty: ");
+        for (String path : paths){
+        	System.out.println(path);
+        	//ss.readXLS(path);
+        }
+        
+        System.out.println("Raporty z lat:");
+        for (String year : timesheets.getListOfYears(args[0])){
+        	System.out.println(year);
+        }
+        
+        Menu menu = new Menu(args[0]);
+        menu.printMenu();
+        
     }
-    
-
+        
 }
